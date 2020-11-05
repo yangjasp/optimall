735927d7426c293e126141754bfc0b5f54ca2969
Jasper Yang <jasper@Jaspers-MacBook-Pro.local>
2020-11-05 14:27:27 -0600

    Update documentation

--- R/optimum_allocation.R
+++ R/optimum_allocation.R
@@ -5,7 +5,11 @@
 #' @param strata a character string or vector of character strings specifying the name(s) of columns which specify the stratum that each unit belongs to. If multiple column names are provided, each unique combination of values in these columns is taken to define one stratum.
 #' @param y a character string specifying the name of the continuous variable for which the variance should be minimized.
 #' @param nsample the desired total sample size. Defaults to NULL.
-#' @param method a character string specifying the method of optimum sample allocation to use. Must be one of \code{"Neyman"}, \code{"WrightI"} or \code{"WrightII"}. Defaults to \code{"WrightII"}.
+#' @param method a character string specifying the method of optimum sample allocation to use. Must be one of:
+#' \itemize{
+#'\item \code{"WrightII"}, the default, uses Algorithm II from Wright (2014) to determine the optimum allocation of a fixed sample size across the strata. It requires that at least two samples are allocated to each stratum, and it is recommended because it is the only algorithm that requires an unbiased estimate of the variance and always produces the global optimum allocation.
+#'\item \code{"WrightI"} uses Wright's Algorithm I to determine the optimum sample allocation. It only requires that at least one sample is allocated to each stratum, and can therefore lead to a biased variance estimate. It is not recommended except for in very specific cases.
+#'\item \code{"Neyman"} uses the standard method of Neyman Allocation to determine the optimum sample allocation. Its calculated stratum sample sizes are frequently non-integer values, but the output rounds these to the nearest integer. It is best used when nsample is `NULL` because it will output exact sampling fractions.
 #' @param ndigits a numeric value specifying the number of digits to round the stratum fraction to.
 #' @param allow.na logical input specifying whether y should be allowed to have NA values. Defaults to \code{FALSE}.
 #' @examples
