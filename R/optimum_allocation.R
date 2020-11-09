205858ef66786a01e04396f24525b4ce9d49f87d
Jasper Yang <jasper@Jaspers-MacBook-Pro.local>
2020-11-09 11:05:56 -0600

    edit doc

--- R/optimum_allocation.R
+++ R/optimum_allocation.R
@@ -10,6 +10,7 @@
 #'\item \code{"WrightII"}, the default, uses Algorithm II from Wright (2014) to determine the optimum allocation of a fixed sample size across the strata. It requires that at least two samples are allocated to each stratum, and it is recommended because it is the only algorithm that requires an unbiased estimate of the variance and always produces the global optimum allocation.
 #'\item \code{"WrightI"} uses Wright's Algorithm I to determine the optimum sample allocation. It only requires that at least one sample is allocated to each stratum, and can therefore lead to a biased variance estimate. It is not recommended except for in very specific cases.
 #'\item \code{"Neyman"} uses the standard method of Neyman Allocation to determine the optimum sample allocation. Its calculated stratum sample sizes are frequently non-integer values, but the output rounds these to the nearest integer. It is best used when nsample is `NULL` because it will output exact sampling fractions.
+#' }
 #' @param ndigits a numeric value specifying the number of digits to round the stratum fraction to.
 #' @param allow.na logical input specifying whether y should be allowed to have NA values. Defaults to \code{FALSE}.
 #' @examples
