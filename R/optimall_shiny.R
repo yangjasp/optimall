aafc994177db3b04048b586bf2b9b095659c18dc
Jasper Yang <jasper@Jaspers-MacBook-Pro.local>
2020-11-12 09:21:34 -0600

    update shiny

--- R/optimall_shiny.R
+++ R/optimall_shiny.R
@@ -3,7 +3,7 @@
 #' Launches an R Shiny application locally. This app can be used to interactively split strata and determine how the results affect optimum allocation of a fixed number of samples.
 #' @export
 optimall_shiny <- function() {
-  appDir <- system.file("shiny-app", "optimall_shiny", "thoroughapp.R", package = "optimall")
+  appDir <- system.file("shiny-app", "optimall_shiny", "app_draft_4.R", package = "optimall")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `optimall`.", call. = FALSE)
     }
