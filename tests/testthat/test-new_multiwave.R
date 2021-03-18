7ba1f5538adf2898fce51efdb0c5e358ea22c953
yangjasp <jbyang@outlook.come>
2021-03-18 11:21:13 -0500

    init

---
+++ tests/testthat/test-new_multiwave.R
@@ -0,0 +1,29 @@
+context("test-new_multiwave")
+
+test <- new_multiwave(phases = 2, waves = c(1,3))
+
+test_that("new_multiwave makes object with proper slots", {
+  expect_equal(length(test@phases), 2)
+  expect_equal(length(test@phases$phase2@waves), 3)
+})
+
+test_that("initiating with metadata and phase1 data works",{
+  test <- new_multiwave(phases = 2, waves = c(1,3),
+                        phase1 = data.frame(a = c("a","b","c"),
+                                            b = c(1,2,3)),
+                        metadata = list(title = "title"))
+  expect_equal(get_data(test, phase = 1), data.frame(a = c("a","b","c"),
+                                                     b = c(1,2,3)))
+  expect_equal(get_data(test, phase = NA, slot = "metadata"),
+               list(title = "title"))
+
+
+})
+
+test_that("errors work",{
+  # Too many waves in phase 1
+  expect_error(new_multiwave(phases = 2, waves = c(2,3)),
+              "phase 1 can only contain")
+  expect_error(new_multiwave(phases = 2, waves = c(2,3,4)),
+               "must be a numeric vector with length")
+})
