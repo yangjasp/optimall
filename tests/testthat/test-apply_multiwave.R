2ba755e3b9fa9ee4190f929a521196766acf649b
yangjasp <jbyang@outlook.come>
2021-03-19 12:05:06 -0500

    Edit doc and tests

--- tests/testthat/test-apply_multiwave.R
+++ tests/testthat/test-apply_multiwave.R
@@ -1,32 +1,52 @@
 context("test-apply_multiwave")
 
+library(optimall)
+library(dplyr)
+
+set.seed <- 345
+iris <- data.frame(id = c(1:60),
+                   Species = rep(c("A","B","C"), times = 20),
+                   Sepal.Length = rnorm(60, 3, 0.7))
+iris$Sepal.Width <- iris$Sepal.Length + rnorm(60, 0, 0.5)
+
 # 1. Optimum_allocation
 
 test_that("optimum_allocation runs with args provided",{
   MySurvey <- new_multiwave(phases = 2, waves = c(1,3))
   get_data(MySurvey, phase = 1, slot = "data") <-
-    dplyr::select(datasets::iris, -Sepal.Width)
+    dplyr::select(iris, -Sepal.Width)
   MySurvey <- apply_multiwave(MySurvey, phase = 2, wave = 1,
     fun = "optimum_allocation", strata = "Species", nsample = 15,
     y = "Sepal.Length",
-    method = "WrightII")
+    method = "WrightII",
+    ndigits = 2,
+    allow.na = FALSE)
   expect_equal(dim(get_data(MySurvey, phase = 2, wave = 1,
                             slot = "design")),
                c(3, 6))
   expect_equal(sum(get_data(MySurvey, phase = 2, wave = 1,
                             slot = "design")$stratum_size),
                15)
+  MySurvey <- apply_multiwave(MySurvey, phase = 2, wave = 1,
+                              fun = "optimum_allocation", strata = "Species",
+                              y = "Sepal.Length",
+                              method = "Neyman")
+  expect_equal(dim(get_data(MySurvey, phase = 2, wave = 1,
+                            slot = "design")),
+               c(3, 5))
 })
 
 test_that("optimum_allocation runs with args in metadata",{
   MySurvey <- new_multiwave(phases = 2, waves = c(1,3))
   get_data(MySurvey, phase = 1, slot = "data") <-
-    dplyr::select(datasets::iris, -Sepal.Width)
+    dplyr::select(iris, -Sepal.Width)
   get_data(MySurvey, phase = NA, slot = "metadata") <- list(
     strata = "Species",
     nsample = 15,
     y = "Sepal.Length",
-    method = "WrightII")
+    method = "WrightII",
+    ndigits = 2,
+    allow.na = FALSE)
   MySurvey <- apply_multiwave(MySurvey, phase = 2, wave = 1,
                               fun = "optimum_allocation")
   expect_equal(dim(get_data(MySurvey, phase = 2, wave = 1,
@@ -41,7 +61,8 @@ test_that("optimum_allocation runs with args in metadata",{
     strata = "Species",
     nsample = 20,
     y = "Sepal.Length",
-    method = "WrightII")
+    method = "WrightII", ndigits = 2,
+    allow.na = FALSE)
   MySurvey <- apply_multiwave(MySurvey, phase = 2, wave = 1,
                               fun = "optimum_allocation")
   expect_equal(dim(get_data(MySurvey, phase = 2, wave = 1,
@@ -53,7 +74,12 @@ test_that("optimum_allocation runs with args in metadata",{
 
   #And wave overrides that
   get_data(MySurvey, phase = 2, wave = 1,
-           slot = "metadata") <- list(nsample = 30)
+           slot = "metadata") <- list(
+             strata = "Species",
+             nsample = 30,
+             y = "Sepal.Length",
+             method = "WrightII", ndigits = 2,
+             allow.na = FALSE)
   MySurvey <- apply_multiwave(MySurvey, phase = 2, wave = 1,
                               fun = "optimum_allocation")
   expect_equal(dim(get_data(MySurvey, phase = 2, wave = 1,
@@ -62,6 +88,22 @@ test_that("optimum_allocation runs with args in metadata",{
   expect_equal(sum(get_data(MySurvey, phase = 2, wave = 1,
                             slot = "design")$stratum_size),
                30)
+})
+
+test_that("optimum_allocation works for wave 2 also",{
+  MySurvey <- new_multiwave(phases = 2, waves = c(1,3))
+  get_data(MySurvey, phase = 2, wave = 1, slot = "data") <- iris
+  MySurvey <- apply_multiwave(MySurvey, phase = 2, wave = 2,
+                              fun = "optimum_allocation",strata = "Species",
+                              nsample = 15,
+                              y = "Sepal.Length",
+                              method = "WrightII",
+                              ndigits = 2,
+                              allow.na = FALSE)
+  expect_equal(dim(get_data(MySurvey, phase = 2, wave = 2,
+                            slot = "design")),
+               c(3, 6))
+
 })
 
 # 2. Allocate_wave
@@ -159,7 +201,11 @@ test_that("allocate_wave runs with args in metadata",{
 
   #And wave overrides that
   get_data(MySurvey, phase = 2, wave = 2,
-           slot = "metadata") <- list(nsample = 33)
+           slot = "metadata") <- list(
+             y = "Sepal.Width",
+             wave2a = "already_sampled_ind",
+             strata = "Species",
+             nsample = 33, detailed = TRUE)
   MySurvey <- apply_multiwave(MySurvey, phase = 2, wave = 2,
                               fun = "allocate_wave")
   expect_equal(dim(get_data(MySurvey, phase = 2, wave = 2,
@@ -170,6 +216,69 @@ test_that("allocate_wave runs with args in metadata",{
                33)
 })
 
+test_that("error if phase = 1",{
+  MySurvey <- new_multiwave(phases = 2, waves = c(1,3))
+  get_data(MySurvey, phase = 1, slot = "data") <-
+    dplyr::select(iris, -Sepal.Width)
+  expect_error(apply_multiwave(MySurvey, phase = 1, wave = 1,
+                               fun = "allocate_wave"),
+               "Allocate wave cannot be performed in Phase 1")
+})
+
+test_that("errors if args are not specified",{
+  MySurvey <- new_multiwave(phases = 2, waves = c(1,3))
+  get_data(MySurvey, phase = 1, slot = "data") <-
+    dplyr::select(iris, -Sepal.Width)
+
+  get_data(MySurvey, phase = 2, slot = "metadata") <- list(
+    strata = "Species",
+    strata2 = "strata",
+    id = "id",
+    n_allocated = "n_to_sample"
+  )
+
+  get_data(MySurvey, phase = 2, wave = 1, slot = "design") <-
+    data.frame(strata = unique(iris$Species), n_to_sample = c(5,5,5))
+  set.seed(123)
+  MySurvey <- apply_multiwave(MySurvey, phase = 2, wave = 1,
+                              fun = "sample_strata")
+
+  samples <- get_data(MySurvey, phase = 2, wave = 1, slot = "samples")
+
+  get_data(MySurvey, phase = 2, wave = 1, slot = "sampled_data") <-
+    dplyr::select(iris, id, Sepal.Width)[samples,]
+  MySurvey <- merge_samples(MySurvey, phase = 2, wave = 1, id = "id",
+                            sampled_ind = "already_sampled_ind")
+  get_data(MySurvey, phase = 2, slot = "metadata") <- list()
+
+  expect_error(apply_multiwave(MySurvey, phase = 2, wave = 2,
+                              fun = "allocate_wave",
+                              y = "Sepal.Width",
+                              wave2a = "already_sampled_ind",
+                              nsample = 30, detailed = TRUE),
+  "must be specified or available in metadata")
+
+  expect_error(apply_multiwave(MySurvey, phase = 2, wave = 2,
+                               fun = "allocate_wave",
+                               y = "Sepal.Width", strata = "Species",
+                               nsample = 30, detailed = TRUE),
+               "must be specified or available in metadata")
+
+
+  expect_error(apply_multiwave(MySurvey, phase = 2, wave = 2,
+                              fun = "allocate_wave",
+                              y = "Sepal.Width",
+                              wave2a = "already_sampled_ind",
+                              strata = "Species", detailed = TRUE),
+              "must be specified or available in metadata")
+
+  expect_error(apply_multiwave(MySurvey, phase = 2, wave = 2,
+                              fun = "allocate_wave",
+                              strata = "Species",
+                              wave2a = "already_sampled_ind",
+                              nsample = 30, detailed = TRUE),
+              "must be specified or available in metadata")
+})
 
 # 3. sample_strata
 
@@ -210,8 +319,24 @@ test_that("sample_strata works with args specified in metadata",{
   expect_equal(length(
     get_data(MySurvey, phase = 2, wave = 1, slot = "samples")), 15)
 
+  # only need to specify strata once if it is same in both
+
+  get_data(MySurvey, phase = NA, slot = "metadata") <- list(
+    strata = "Species",
+    id = "id",
+    n_allocated = "n_to_sample"
+  )
+
+  get_data(MySurvey, phase = 2, wave = 1, slot = "design") <-
+    data.frame(Species = unique(iris$Species), n_to_sample = c(5,5,5))
+  set.seed(123)
+  MySurvey <- apply_multiwave(MySurvey, phase = 2, wave = 1,
+                              fun = "sample_strata")
+  expect_equal(length(
+    get_data(MySurvey, phase = 2, wave = 1, slot = "samples")), 15)
+
   #but metadata in phase overrides
-  get_data(MySurvey, slot = "metadata") <- list(
+  get_data(MySurvey, phase = 2, slot = "metadata") <- list(
     strata = "Species",
     strata2 = "strata",
     id = "id",
@@ -220,6 +345,22 @@ test_that("sample_strata works with args specified in metadata",{
 
   get_data(MySurvey, phase = 2, wave = 1, slot = "design") <-
     data.frame(strata = unique(iris$Species), n_to_sample = c(6,5,5))
+  set.seed(123)
+  MySurvey <- apply_multiwave(MySurvey, phase = 2, wave = 1,
+                              fun = "sample_strata")
+  expect_equal(length(
+    get_data(MySurvey, phase = 2, wave = 1, slot = "samples")), 16)
+
+  # again, only need to specify strata once if it is same in both
+
+  get_data(MySurvey, phase = 2, slot = "metadata") <- list(
+    strata = "Species",
+    id = "id",
+    n_allocated = "n_to_sample"
+  )
+
+  get_data(MySurvey, phase = 2, wave = 1, slot = "design") <-
+    data.frame(Species = unique(iris$Species), n_to_sample = c(6,5,5))
   set.seed(123)
   MySurvey <- apply_multiwave(MySurvey, phase = 2, wave = 1,
                               fun = "sample_strata")
@@ -241,6 +382,92 @@ test_that("sample_strata works with args specified in metadata",{
                               fun = "sample_strata")
   expect_equal(length(
     get_data(MySurvey, phase = 2, wave = 1, slot = "samples")), 17)
+
+  # only need to specify strata once if it is same in both
+
+  get_data(MySurvey, phase = 2, wave = 1, slot = "metadata") <- list(
+    strata = "Species",
+    id = "id",
+    n_allocated = "n_to_sample"
+  )
+
+  get_data(MySurvey, phase = 2, wave = 1, slot = "design") <-
+    data.frame(Species = unique(iris$Species), n_to_sample = c(7,5,5))
+  set.seed(123)
+  MySurvey <- apply_multiwave(MySurvey, phase = 2, wave = 1,
+                              fun = "sample_strata")
+  expect_equal(length(
+    get_data(MySurvey, phase = 2, wave = 1, slot = "samples")), 17)
+})
+
+test_that("errors in sample_strata if args don't match",{
+  MySurvey <- new_multiwave(phases = 2, waves = c(1,3))
+  get_data(MySurvey, phase = 1, slot = "data") <-
+    dplyr::select(iris, -Sepal.Width)
+
+  # If empty design
+  get_data(MySurvey, phase = 2, wave = 1, slot = "design") <-
+    data.frame()
+  set.seed(123)
+  expect_error(apply_multiwave(MySurvey, phase = 2, wave = 1,
+                               fun = "sample_strata",strata1 = "Species",
+                               strata2 = "strata",
+                               id = "id",
+                               n_allocated = "n_to_sample"),
+               "of specified wave must be filled")
+
+  # If empty data
+  get_data(MySurvey, phase = 1, slot = "data") <-
+    data.frame()
+
+  get_data(MySurvey, phase = 2, wave = 1, slot = "design") <-
+    data.frame(strata = unique(iris$Species), n_to_sample = c(5,5,5))
+
+
+  expect_error(apply_multiwave(MySurvey, phase = 2, wave = 1,
+                               fun = "sample_strata",strata1 = "Species",
+                               strata2 = "strata",
+                               id = "id",
+                               n_allocated = "n_to_sample"),
+               "of previous wave must contain data to be used")
+
+  get_data(MySurvey, phase = 1, slot = "data") <-
+    dplyr::select(iris, -Sepal.Width)
+
+  expect_error(apply_multiwave(MySurvey, phase = 2, wave = 1,
+                               fun = "sample_strata", strata = NULL,
+                               strata2 = "strata",
+                               id = "id",
+                               n_allocated = "n_to_sample"),
+               "must be specified")
+  expect_error(apply_multiwave(MySurvey, phase = 2, wave = 1,
+                               fun = "sample_strata",strata1 = "Species",
+                               strata2 = "wrong",
+                               id = "id",
+                               n_allocated = "n_to_sample"),
+               "must be a column name")
+  expect_error(apply_multiwave(MySurvey, phase = 2, wave = 1,
+                               fun = "sample_strata",strata1 = "Species",
+                               strata2 = "strata", id = NULL,
+                               n_allocated = "n_to_sample"),
+               "must be specified or available")
+  expect_error(apply_multiwave(MySurvey, phase = 2, wave = 1,
+                               fun = "sample_strata",strata1 = "Species",
+                               strata2 = "strata",
+                               wave2a = NULL,
+                               id = "id",
+                               n_allocated = "wrong"),
+               "must be a column name")
+  expect_error(apply_multiwave(MySurvey, phase = 2, wave = 1,
+                               fun = "sample_strata",strata1 = "Species",
+                               strata2 = "strata",
+                               wave2a = "wrong",
+                               id = "id"),
+               "must be a column name")
+
+
+
+
 })
 
 # 4. merge_samples
@@ -277,7 +504,7 @@ test_that("merge_samples works when args are specified within it",{
                               sampled_ind = "already_sampled_ind")
 
   expect_equal(dim(MySurvey@phases$phase2@waves$wave1@data),
-               c(150,10))
+               c(60,5))
   expect_equal(length(
     MySurvey@phases$phase2@waves$wave1@data$`already_sampled_ind`[
       MySurvey@phases$phase2@waves$wave1@data$`already_sampled_ind` == 1
@@ -294,7 +521,7 @@ test_that("merge_samples works with args specifies in metadata",{
   get_data(MySurvey, phase = 1, slot = "data") <-
     dplyr::select(iris, -Sepal.Width)
 
-  get_data(MySurvey, phase = 2, slot = "metadata") <- list(
+  get_data(MySurvey, phase = NA, slot = "metadata") <- list(
     strata = "Species",
     strata2 = "strata",
     id = "id",
@@ -323,7 +550,7 @@ test_that("merge_samples works with args specifies in metadata",{
                               fun = "merge_samples")
 
   expect_equal(dim(MySurvey@phases$phase2@waves$wave1@data),
-               c(150,10))
+               c(60,5))
   expect_equal(length(
     MySurvey@phases$phase2@waves$wave1@data$`already_sampled_ind`[
       MySurvey@phases$phase2@waves$wave1@data$`already_sampled_ind` == 1
@@ -342,7 +569,7 @@ test_that("merge_samples works with args specifies in metadata",{
                               fun = "merge_samples")
 
   expect_equal(dim(MySurvey@phases$phase2@waves$wave1@data),
-               c(150,10))
+               c(60,5))
   expect_equal(length(
     MySurvey@phases$phase2@waves$wave1@data$`already_sampled_ind1`[
       MySurvey@phases$phase2@waves$wave1@data$`already_sampled_ind1` == 1
@@ -361,7 +588,7 @@ test_that("merge_samples works with args specifies in metadata",{
                               fun = "merge_samples")
 
   expect_equal(dim(MySurvey@phases$phase2@waves$wave1@data),
-               c(150,10))
+               c(60,5))
   expect_equal(length(
     MySurvey@phases$phase2@waves$wave1@data$`already_sampled_ind2`[
       MySurvey@phases$phase2@waves$wave1@data$`already_sampled_ind2` == 1
