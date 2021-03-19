39315fd927b9af8abb77faf90e803d0032664e55
yangjasp <jbyang@outlook.come>
2021-03-19 12:06:07 -0500

    Add error tests

--- tests/testthat/test-merge_samples.R
+++ tests/testthat/test-merge_samples.R
@@ -6,6 +6,12 @@ library(optimall)
 #Make multiwave_object
 test <- new_multiwave(phases = 2, waves = c(1,3))
 
+set.seed <- 345
+iris <- data.frame(id = c(1:60),
+                   Species = rep(c("A","B","C"), times = 20),
+                   Sepal.Length = rnorm(60, 3, 0.7))
+iris$Sepal.Width <- iris$Sepal.Length + rnorm(60, 0, 0.5)
+
 get_data(test, phase = 1, slot = "data") <-
   dplyr::select(iris, -Sepal.Width)
 
@@ -38,7 +44,7 @@ test <- merge_samples(test, phase = 2, wave = 1, id = "id",
 test_that("merge_samples merges data with sampled data
           when column doesn't already exist", {
   expect_equal(dim(test@phases$phase2@waves$wave1@data),
-               c(150,10))
+               c(60,5))
   expect_equal(length(
     test@phases$phase2@waves$wave1@data$`already_sampled_ind`[
       test@phases$phase2@waves$wave1@data$`already_sampled_ind` == 1
@@ -50,7 +56,7 @@ test_that("merge_samples merges data with sampled data
 })
 
 #Testing Wave 2
-samples2 <- sample(c(1:150)[-as.numeric(samples)], 15)
+samples2 <- sample(c(1:60)[-as.numeric(samples)], 15)
 # No duplicates
 get_data(test, phase = 2, wave = 2, slot = "samples") <-
   as.character(samples2) #No duplicates
@@ -62,7 +68,7 @@ test1 <- merge_samples(test, phase = 2, wave = 2, id = "id",
 test_that("merge_samples merges data with sampled data
           when column already exists", {
             expect_equal(dim(test1@phases$phase2@waves$wave1@data),
-                         c(150,10))
+                         c(60,5))
             expect_equal(length(
               test1@phases$phase2@waves$wave2@data$`already_sampled_ind`[
                 test1@phases$phase2@waves$wave2@data$`already_sampled_ind` == 1
@@ -90,16 +96,16 @@ test_that("warning if new ids are in sampled data",{
   temp <- test1
   get_data(temp, phase = 2, wave = 2, slot = "sampled_data") <-
     rbind(dplyr::select(iris, id, Sepal.Width)[samples2,],
-          c(160, 3.5))
+          c(61, 3.5))
   #Make dup
   get_data(temp, phase = 2, wave = 2, slot = "samples") <-
-    c(as.character(samples2), "160") #Make dup
+    c(as.character(samples2), "61") #Make dup
   expect_warning(temp <- merge_samples(temp, phase = 2, wave = 2, id = "id",
                                sampled_ind = "already_sampled_ind"),
                  "sampled_data is introducing new ids")
   #temp <- merge_samples(temp, phase = 2, wave = 2, id = "id",
                         #sampled_ind = "already_sampled_ind")
-  expect_equal(nrow(get_data(temp, 2, 2, "data")),151) #one extra row
+  expect_equal(nrow(get_data(temp, 2, 2, "data")),61) #one extra row
 })
 
 test_that("warning if one of wave's 'samples' slots is empty",{
@@ -116,29 +122,52 @@ test_that("warning if one of wave's 'samples' slots is empty",{
 
 test_that("arguments can be specified in metadata",{
   temp <- test1
-  get_data(temp, phase = 2, wave = 2, "metadata") <- list(
+  get_data(test1, phase = NA, wave = NA, "metadata") <- list(
     id = "id",
     sampled_ind = "already_sampled_ind"
   )
   temp <- merge_samples(temp, phase = 2, wave = 2)
-  expect_equal(dim(get_data(temp, 2, 2, "data")),c(150,10))
+  expect_equal(dim(get_data(temp, 2, 2, "data")),c(60,5))
+  expect_equal("already_sampled_ind" %in% names(
+    get_data(temp, phase = 2, wave = 2, slot = "data")),
+    TRUE
+  )
   temp <- test1
   get_data(temp, phase = 2, wave = NA, "metadata") <- list(
     id = "id",
     sampled_ind = "already_samples_ind"
   )
   temp <- merge_samples(temp, phase = 2, wave = 2)
-  expect_equal(dim(get_data(temp, 2, 2, "data")),c(150,10))
+  expect_equal(dim(get_data(temp, 2, 2, "data")),c(60,5))
   temp <- test1
-  get_data(test1, phase = NA, wave = NA, "metadata") <- list(
+  get_data(temp, phase = 2, wave = 2, "metadata") <- list(
     id = "id",
     sampled_ind = "already_sampled_ind"
   )
   temp <- merge_samples(temp, phase = 2, wave = 2)
-  expect_equal(dim(get_data(temp, 2, 2, "data")),c(150,10))
-  expect_equal("already_sampled_ind" %in% names(
-    get_data(temp, phase = 2, wave = 2, slot = "data")),
-    TRUE
-  )
+  expect_equal(dim(get_data(temp, 2, 2, "data")),c(60,5))
+
+
+})
+
+test_that("Errors as necessary",{
+  temp <- test1
+  get_data(temp, phase = 2, wave = 2, slot = "sampled_data") <-
+    dplyr::select(iris, id, Sepal.Width)[samples2,]
+  #Make dup
+  get_data(temp, phase = 2, wave = 2, slot = "samples") <-
+    character(0) #forget to specify samples
+  expect_error(merge_samples(temp, phase = 0, wave = 2, id = "id",
+                                       sampled_ind = "already_sampled_ind"),
+                 "must be a numeric value specifying a valid phase")
+  expect_error(merge_samples(temp, phase = 2, wave = 5, id = "id",
+                                     sampled_ind = "already_sampled_ind"),
+               "must be a numeric value specifying a valid wave")
+  expect_error(merge_samples(temp, phase = 2, wave = 2, id = "id",
+                             sampled_ind = 3),
+               "must be a character value")
+  expect_error(merge_samples(temp, phase = 2, wave = 2, id = "wrong",
+                             sampled_ind = 3),
+               "must be a character value")
 
 })
