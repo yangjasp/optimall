416d7a563e33c7825c75350557c3399f7457ddce
yangjasp <jbyang@outlook.come>
2021-03-19 12:01:02 -0500

    Revise Rd

--- R/merge_samples.R
+++ R/merge_samples.R
@@ -36,16 +36,20 @@
 #' @return A Multiwave object with the merged dataframe in the
 #' \code{"data"} slot of the specified wave.
 #' @export
-#' @include multiwave.R
+#' @include multiwave.R phase.R wave.R
+#' @aliases merge_samples
 #'
 #' @examples
+#' library(datasets)
+#' iris <- data.frame(iris, id = 1:150)
+#'
 #' MySurvey <- new_multiwave(phases = 2, waves = c(1,3))
 #' get_data(MySurvey, phase = 1, slot = "data") <-
-#' dplyr::select(iris, -Sepal.Width)
+#' data.frame(dplyr::select(iris, -Sepal.Width))
 #' get_data(MySurvey, phase = 2, wave = 1, slot = "sampled_data") <-
 #' dplyr::select(iris, id, Sepal.Width)[1:40,]
 #' MySurvey <- merge_samples(MySurvey, phase = 2, wave = 1, id = "id")
-#'
+#' @importFrom rlang :=
 setGeneric("merge_samples", function(x, phase, wave,
                                   id = NULL,
                                   sampled_ind = "already_sampled_ind")
