8a7826b1ebe98c12fd25a9cfec9f1d0f58e6f60b
yangjasp <jbyang@outlook.come>
2021-04-07 13:21:19 -0500

    improve test coverage

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -7,6 +7,8 @@ get_data(MySurvey, phase = 1, slot = "data") <-
   data.frame(a = c(1,2,3,4), b = c(2,3,4,5))
 get_data(MySurvey, phase = 2, wave = 2, slot = "data") <-
   data.frame(a = c(1,2,3), b = c(2,3,4))
+get_data(MySurvey, phase = 2, wave = 2, slot = "sampled_data") <-
+  data.frame(a = c(1,2,3), b = c(2,3,4))
 
 test_that("summary works", {
   expect_output(summary(MySurvey), "object with 2 phases containing 1, 2 wav")
