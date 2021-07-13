a0b271dd47eff2c5fff901b80ee7a03e92d5c6bd
yangjasp <jbyang@outlook.come>
2021-07-13 16:23:07 -0400

    option to pass args to runApp

--- R/optimall_shiny.R
+++ R/optimall_shiny.R
@@ -3,15 +3,18 @@
 #' Launches an R Shiny application locally. This app can be used to
 #' interactively split strata and determine how the results affect
 #' optimum allocation of a fixed number of samples.
+#' @param ... Optional arguments to pass to \code{shiny::runApp}.
+#' \code{display.mode} is already set to normal.
+#' @return Launches an R Shiny application locally.
 #' @export
-optimall_shiny <- function() {
+optimall_shiny <- function(...) {
   appDir <- system.file("shiny-app", "optimall_shiny",
     "app_draft_4.R",
     package = "optimall"
   )
   if (appDir == "") {
-    stop("Could not find example directory.
+    stop("Could not find directory.
          Try re-installing `optimall`.", call. = FALSE)
   }
-  shiny::runApp(appDir, display.mode = "normal")
+  shiny::runApp(appDir, display.mode = "normal", ...)
 }
