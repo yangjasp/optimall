608c9abe9e89c2df3dd8b3402e9195bada730318
yangjasp <jbyang@outlook.come>
2021-07-19 13:46:57 -0400

    skip for linuz without Phantomjs

--- tests/testthat/test-optimall_shiny.R
+++ tests/testthat/test-optimall_shiny.R
@@ -2,15 +2,34 @@ context("test-optimall_shiny")
 
 library(optimall)
 library(shinytest)
+library(globals)
+library(webshot)
+
+# Sometimes it may be useful to skip if not interactive
+# skip_if_not(interactive())
 
 # Set up app
+
+# phantomJS is required to run this test. This function will install it
+# if possible, but it doesn't work on Fedora.
+skip_on_os("linux")
+
+if (!dependenciesInstalled()){
+  shinytest::installDependencies()
+}
+
+# If it fails for some reason, just skip the test.
+skip_if_not(webshot::is_phantomjs_installed())
+
 app <- shinytest::ShinyDriver$new(shiny::shinyApp(ui = shiny_ui,
-                                                  server = shiny_server))
+                                                  server = shiny_server),
+                                  loadTimeout = 1000000)
 
 test_that("App runs", {
 
    # accepts data
-  app$uploadFile(data = system.file("data","MatWgt_Sim.rda", package = "optimall"))
+  #app$uploadFile(data = file.path(system.file("data", package = "optimall"), "MatWgt_Sim.rda"))
+  app$uploadFile(data = system.file("extdata", "MatWgt_Sim.csv", package = "optimall"))
 
   # set inputs
   app$setInputs(strata =  "race", split_at = 0.5, split_var = "mat_weight_est",
@@ -19,9 +38,10 @@ test_that("App runs", {
   app$setInputs(strata_to_split = "White")
   app$click("confirm")
 
-  output_text <- app$getValue("list")
+  values <- app$getAllValues()
 
-  expect_equal(length(output_text), 1)
+  expect_equal(nchar(values$output$list)[1], 141)
+  expect_equal(length(values$output$list), 1)
 })
 
 # stop app
