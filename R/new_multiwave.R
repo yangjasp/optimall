54839c0414b7912ed8cefe4931782c891bfaea7a
yangjasp <jbyang@outlook.come>
2024-03-14 12:15:08 -0700

    Rename initial function multiwave()

--- R/new_multiwave.R
+++ R/new_multiwave.R
@@ -22,20 +22,65 @@
 #'
 #' # Initialize a multiwave object for a two-phase sampling design that will
 #' # sample over three waves in the second phase
-#' multiwave_object <- new_multiwave(phases = 2, waves = c(1, 3))
+#' multiwave_object <- multiwave(phases = 2, waves = c(1, 3))
 #'
 #' # If we already have the phase 1 data and want to add a title to the survey
 #' # metadata, we can initialize the object with these included.
 #'
 #' library(datasets)
-#' multiwave_object <- new_multiwave(
+#' multiwave_object <- multiwave(
 #'   phases = 2, waves = c(1, 3),
 #'   metadata = list(title = "my two-phase survey"), phase1 = iris
 #' )
 #' @export
-
 new_multiwave <- function(phases, waves,
                           metadata = list(), phase1 = data.frame()) {
+  if (!is.double(phases) | is.double(phases) & phases < 1) {
+    stop("'phases' must be a numeric value >= 1")
+  }
+  if (!is.double(waves) | is.double(waves) & length(waves) != phases) {
+    stop("'waves' must be a numeric vector with length matching
+         the number of phases specified in 'phases'")
+  }
+  if (waves[1] != 1) {
+    stop("phase 1 can only contain one wave")
+  }
+  # build list of metadata and waves for each specified phase
+  if (phases == 1) {
+    phases_list <- list(phase1 = list(
+      "metadata" = list(),
+      data = phase1
+    ))
+  } else if (phases > 1) {
+    phases_list <- list()
+    phase_names <- vector()
+    for (i in 2:phases) {
+      phase_names[i - 1] <- paste0("phase", as.character(i))
+      n_waves <- waves[i]
+      waves_list <- list()
+      wave_names <- vector()
+      for (j in 1:n_waves) {
+        wave_names[j] <- paste0("wave", as.character(j))
+        waves_list[[j]] <- new("Wave")
+      }
+      names(waves_list) <- wave_names
+      the_phase <- new("Phase", waves = waves_list)
+      phases_list[[i]] <- the_phase
+    }
+    phases_list[[1]] <- list(
+      "metadata" = list(),
+      data = phase1
+    )
+    names(phases_list) <- c("phase1", phase_names)
+  }
+  output <- new("Multiwave", metadata = metadata, phases = phases_list)
+  return(output)
+}
+
+#' @export
+new_multiwave <- function(phases, waves,
+                          metadata = list(), phase1 = data.frame()) {
+  warning("new_multiwave() is deprecated. Please use multiwave() instead")
   if (!is.double(phases) | is.double(phases) & phases < 1) {
     stop("'phases' must be a numeric value >= 1")
   }
